413c38db7baa3bb35eb0527a5fda050e4ea8c26a
MUKILTEO\1720941 <1720941>
2026-05-11 10:25:32 -0700

    car

---
+++ My project/Assets/People's/Jacobe/CarScripts/Wheel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    public WheelCollider wheelCollider;
+    public Transform wheelMesh;
+    public bool wheelTurn;
+    public bool wheelSuspensionFollow;
+
+    void Update()
+    {
+        if (wheelCollider == null || wheelMesh == null)
+        {
+            return;
+        }
+
+        Vector3 worldPos;
+        Quaternion worldRot;
+        wheelCollider.GetWorldPose(out worldPos, out worldRot);
+
+        if (wheelSuspensionFollow)
+        {
+            wheelMesh.position = worldPos;
+        }
+
+        if (wheelTurn)
+        {
+            Vector3 localEuler = wheelMesh.localEulerAngles;
+            float steerYaw = wheelCollider.steerAngle;
+            wheelMesh.localEulerAngles = new Vector3(localEuler.x, steerYaw, localEuler.z);
+        }
+
+        float spinDegrees = wheelCollider.rpm / 60f * 360f * Time.deltaTime;
+        wheelMesh.Rotate(spinDegrees, 0f, 0f, Space.Self);
+    }
+}
